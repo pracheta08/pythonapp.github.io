116f33c207c9edcb8ec9e5987a1573f063cbd8e2
pracheta08 <30788826+pracheta08@users.noreply.github.com>
2017-08-19 05:48:14 -0700

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-#!usr/bin/python
-
-import cgi
-form = cgi.FieldStorage()
-with open ('fileToWrite.txt','w') as fileOutput:
-    fileOutput.write(form.getValue('firstname'))
-    fileOutput.write(form.getValue('lastname'))
